6607e07cdb39d293808cd99bdd7a41d5c9a339e4
osimen grace <osimenobehi6@gmail.com>
2024-07-26 15:50:28 +0100

    block

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
-/* eslint-disable */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
+ /* eslint-disable */
     task = true;
     task2 = false;
+  /* eslint-enable */
   }
 
   return [task, task2];
